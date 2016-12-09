66dd4653dd8302704265e5d7c28d4c17d3935eff
daryl.behrens <daryl.behrens@DARYLBEHRENS-PC>
2016-12-09 13:01:09 -0800

    Updated Build

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 cls
-"nuget.exe" "Install" "FAKE" "-OutputDirectory" "src\packages" "-ExcludeVersion"
+"C:\tools\nuget\nuget.exe" "Install" "FAKE" "-OutputDirectory" "src\packages" "-ExcludeVersion"
 "src\packages\FAKE\tools\Fake.exe" build.fsx
